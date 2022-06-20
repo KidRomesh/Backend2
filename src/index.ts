aa78c39947a972990612847c1284a3a1517af2c3
KidRomesh <romikhanna1301@gmail.com>
2022-06-20 22:30:31 +0530

    heroku

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,32 @@ import employee from "./Modules/Employees/employee_routes";
 import auth from "./Modules/Auth/auth"
 import task from "./Modules/Task/task_routes"
 import config from "./config/config";
+import { DataSource } from "typeorm";
+import { Bg } from "./entity/Bg";
+import { Department } from "./entity/Dept";
+import { Employee } from "./entity/Employee";
+import { Gender } from "./entity/Gender";
+import { Individualinfo } from "./entity/Individualinfo";
+import { Role } from "./entity/Role";
+import { Student } from "./entity/Student";
+import { Task } from "./entity/Task";
+
+const connect =new DataSource({
+    type: "mongodb",
+    // database: "Backend",
+    url: "mongodb+srv://KidRomesh:Rdk2001@cluster0.wlumh3o.mongodb.net/test",
+    useNewUrlParser:true,
+    synchronize: false,
+    logging: true,
+    useUnifiedTopology: true,
+    entities: [Employee, Student, Department, Individualinfo, Bg, Role, Gender, Task],
+    migrations: [],
+    subscribers: [],
+})
 
 
-AppDataSource.initialize().then(async () => {
+
+connect.initialize().then(async () => {
     const app: Express = express();
     app.use(express.urlencoded({ extended: true }));
     app.use(morgan("dev"));
